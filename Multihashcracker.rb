c6fa1852f29bcd82653a7a23a237eff4f8859d45
PerhapsSomeone <ma2003br@gmail.com>
2017-11-03 13:22:01 +0100

    Multihashcracker stores cracked hashes in logfile

--- Multihashcracker.rb
+++ Multihashcracker.rb
@@ -13,11 +13,13 @@ hashes[1] = gets.chomp.downcase
 print "Hashtype: "
 Mode = gets.chomp
 else 
-hashes = ARGV[1..9]
+hashes = ARGV[1..100000]
+puts "Inserting following hashes:"
 puts hashes
 Mode = ARGV[0]
 end
 
+logfile = File.open("Cracked.txt","a+")
 counter = 0
         puts "Starting Cracker"
         sleep(1)
@@ -27,19 +29,29 @@ counter = 0
                  tryhash = Digest::MD5.hexdigest(counter.to_s)
                when "SHA1"
                  tryhash = Digest::SHA1.hexdigest(counter.to_s)
+               when "SHA256"
+                 tryhash = Digest::SHA256.hexdigest(counter.to_s)
+               when "SHA384"
+                 tryhash = Digest::SHA384.hexdigest(counter.to_s) 
+               when "SHA512" 
+                 tryhash = Digest::SHA512.hexdigest(counter.to_s)
+               when "RIPEMD-160"
+                 tryhash = Digest::RMD160.hexdigest(counter.to_s)
                end
               if (hashes[0].to_s == tryhash.to_s)
               puts "Match for #{hashes[0]} found! #{counter}"
               matchfound = true
             end
-        counter += 1
-        counter = 0 if matchfound
         if matchfound == true
            hashes.delete_at(0)
            #puts "ArrDel"
+           logfile.puts("#{tryhash} #{counter} \n")
            matchfound = false
+           counter = 0
         end
+        logfile.close if hashes.empty?
         exit if hashes.empty?
+        counter += 1 
         #puts "#{hashes[0]} #{counter} " #uncomment to make verbose
         end
 

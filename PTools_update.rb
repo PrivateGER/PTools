b1baa18deb4a6031fece72bbb63fc3b394ea6c97
PerhapsSomeone <ma2003br@gmail.com>
2017-11-04 22:48:29 +0100

    Added ability to update PTools directly from Lauchner using git pull

---
+++ PTools_update.rb
@@ -0,0 +1,10 @@
+system "clear" or system "cls"
+puts "Updating...."
+if (system('git pull') == true)
+  puts "Update successful!"
+else
+  puts "Update failed! Please check your internet connection and make sure Git is installed."
+end
+puts "Press ENTER to continue..."
+gets
+load 'PTools.rb'

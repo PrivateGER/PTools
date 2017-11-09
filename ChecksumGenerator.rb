5ae840c972031e318a2a3a7ac6b3572cf775b20a
PerhapsSomeone <ma2003br@gmail.com>
2017-11-09 16:54:19 +0100

    Fixed indentation

--- ChecksumGenerator.rb
+++ ChecksumGenerator.rb
@@ -9,11 +9,11 @@ print "Mode: "
 mode = gets.chomp.downcase #downcases input to make it case-insensitive
 case mode
 when "md5" #case insensitive mode conditions
-checksum = Digest::MD5.hexdigest(filecontent) #assign hash to variable
+    checksum = Digest::MD5.hexdigest(filecontent) #assign hash to variable
 when "sha1"
-checksum = Digest::SHA1.hexdigest(filecontent) #assign hash to variable
+    checksum = Digest::SHA1.hexdigest(filecontent) #assign hash to variable
 when "sha512"
-checksum = Digest::SHA512.hexdigest(filecontent) #assign hash to variable
+    checksum = Digest::SHA512.hexdigest(filecontent) #assign hash to variable
 end
 puts "Checksum is: #{checksum}"
 puts "Press ENTER to continue..."

51e923368c8d47b8f8a43519dcb2486650865e69
PerhapsSomeone <ma2003br@gmail.com>
2017-11-13 17:55:00 +0100

    Added new cloned-websites directory which all cloned sites are kept in

--- WebsiteCloner.rb
+++ WebsiteCloner.rb
@@ -2,7 +2,7 @@ system "cls" or system "clear"
 puts "Website-Cloner"
 print "Enter URL/IP: "
 target = gets.chomp
-system "wget --mirror -p --convert-links -P ./ #{target}"
+system "wget --mirror -p --convert-links -P ./cloned-websites/ #{target}"
 puts "Done!"
 puts "Press ENTER to continue..."
 gets
